a5e88d3298b8fe8794fd88fd108d1fbb0679de9b
Bryan Holdt <BHoldt@paraport.com>
2015-07-13 10:27:05 -0700

    Removed useless runs.

--- gem_runner.rb
+++ gem_runner.rb
@@ -3,8 +3,11 @@ require_relative 'lib/commit'
 
 class Runner
   hg = HolyGit.new
-  puts "Getting last local commit..."
-  hg.print_last_local_commit
-  puts "Getting last remote commit..."
-  hg.print_last_remote_commit(HolyGit.execute_github_get_request)
+  # puts "Getting last local commit..."
+  # hg.print_last_local_commit
+  # puts "Getting last remote commit..."
+  # hg.print_last_remote_commit(HolyGit.execute_github_get_request)
+
+  puts "Checking if local is behind..."
+  puts hg.is_local_behind_remote?
 end
