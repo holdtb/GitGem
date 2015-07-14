4e1ea068761a0928fe9c544eab698cefefc12318
Bryan Holdt <BHoldt@paraport.com>
2015-07-14 09:44:58 -0700

    Added regex tests

--- lib/holy_git.rb
+++ lib/holy_git.rb
@@ -6,6 +6,9 @@ require_relative './git_parser'
 require_relative './rspec_parser'
 class HolyGit
 
+
+  # todo DELETE THIS FILE
+
   def initialize
     @git_repo = Git.open(Dir.pwd)
   end
--- lib/parsers/git_parser.rb
+++ lib/parsers/git_parser.rb
@@ -2,9 +2,8 @@ class GitParser
 
   @file_modified_regex = /Changes not staged for commit/
 
-   def self.parse_local_repo_has_changes?(git_status)
-    @file_modified_regex =~ (git_status)
+  def self.parse_local_repo_has_changes?(git_status)
+    (@file_modified_regex =~ (git_status)).nil? ? false : true
   end
-  
-end
 
+end
